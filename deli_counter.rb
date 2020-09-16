a67ad2d5bb4af2e3c576c75f65a0b12c658e87c0
Latisha Joe <latisha_joe@yahoo.com>
2020-09-16 09:45:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 katz_deli= [ ]
 
 def line(katz_deli)
- if katz_deli.length >= 1 
+ if katz_deli.length >= 1
    new_array = [ ]
-   counter = 1 
-   katz_deli.each do |name| 
+   counter = 1
+   katz_deli.each do |name|
    new_array << (" #{counter}. #{name}")
    counter += 1
  end
   puts "The line is currently: #{new_array.join(" ")}"
- else 
+ else
   puts "The line is currently empty"
- end   
+ end
 end
